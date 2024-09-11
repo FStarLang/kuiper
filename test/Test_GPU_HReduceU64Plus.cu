3f29561be5c822425aa318e78db55d33e8febbf1
Guido Martínez <mtzguido@gmail.com>
2024-09-10 22:49:35 -0700

    test

--- test/Test_GPU_HReduceU64Plus.cu
+++ test/Test_GPU_HReduceU64Plus.cu
@@ -2,17 +2,30 @@
 #include <stdint.h>
 #include "GPU_HReduceU64Plus.h"
 
+/* It would be nicer to write a purely-Pulse test. */
 int main()
 {
-	uint64_t a[1024];
+	uint64_t *a;
+	uint64_t *ga;
+	const size_t siz = GPU_HReduceU64Plus_size;
+
+	a = (uint64_t*)malloc(siz * sizeof(uint64_t));
+	ga = (uint64_t*)PULSE_GPU_ALLOC(siz * sizeof(uint64_t));
+
 	int i;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 0; i < siz; i++)
 		a[i] = i;
 
-	uint64_t r = GPU_HReduceU64Plus_reduce(a);
+	MUST(cudaMemcpy(ga, a, siz * sizeof(uint64_t), cudaMemcpyHostToDevice));
 
-	printf("%lu\n", r);
+	PULSE_KCALL(GPU_HReduceU64Plus_k_reduce, 1, siz, siz, ga);
+
+	MUST(cudaMemcpy(a, ga, siz * sizeof(uint64_t), cudaMemcpyDeviceToHost));
+	MUST(cudaFree(ga));
+
+	printf("%lu\n", a[0]);
+	free(a);
 
 	return 0;
 }
