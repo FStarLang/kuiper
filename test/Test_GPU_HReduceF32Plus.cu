75dc2cc4a8a1db23a2bdac35004e3142c4d127ca
Guido Martínez <mtzguido@gmail.com>
2024-09-12 21:48:47 -0700

    one more

---
+++ test/Test_GPU_HReduceF32Plus.cu
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "GPU_HReduceF32Plus.h"
+
+/* It would be nicer to write a purely-Pulse test. */
+int main()
+{
+	float *a;
+	float *ga;
+	const size_t siz = GPU_HReduceF32Plus_size;
+
+	a = (float*)malloc(siz * sizeof(float));
+	ga = (float*)PULSE_GPU_ALLOC(siz * sizeof(float));
+
+	int i;
+
+	for (i = 0; i < siz; i++)
+		a[i] = i;
+
+	MUST(cudaMemcpy(ga, a, siz * sizeof(float), cudaMemcpyHostToDevice));
+
+	PULSE_KCALL(GPU_HReduceF32Plus_k_reduce, 1, siz, siz, ga);
+
+	MUST(cudaMemcpy(a, ga, siz * sizeof(float), cudaMemcpyDeviceToHost));
+	MUST(cudaFree(ga));
+
+	printf("%lf\n", a[0]);
+	free(a);
+
+	return 0;
+}
