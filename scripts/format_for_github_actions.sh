bc9488756ce688f0abfd6a2144ba8de6f2bfc7b6
Guido Martínez <mtzguido@gmail.com>
2024-10-08 09:05:56 -0700

    tweak

--- scripts/format_for_github_actions.sh
+++ scripts/format_for_github_actions.sh
@@ -5,17 +5,15 @@ set -eu
 # Call this with no arguments and feeding a build log to stdin,
 # or with arguments that are filenames to buildogs.
 
-cat "$@" | while IFS= read msg; do
-  if ! echo "$msg" | grep -q '^{'; then
-    continue
-  fi
+grep '^{' "$@" | while IFS= read -r msg; do
   level=$(jq -r .level <<< $msg)
   file=$(jq -r .range.def.file_name <<< $msg)
   # -m: do not fail if file does not exist
   file=$(realpath -m --relative-to=. "${file}")
   line=$(jq -r .range.def.start_pos.line <<< $msg)
   endLine=$(jq -r .range.def.end_pos.line <<< $msg)
-  body=$(cat <<< $msg | jq -r '.msg | join (". ")' | tr '\n' ' ')
+  body=$(cat <<< "$msg" | jq -r '.msg | join ("; ")')
+  body=$(tr '\n' ' ' <<< $body)
   if [ "${level}" == "Error" ]; then
     glevel=error
   elif [ "${level}" == "Warning" ]; then
