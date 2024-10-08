52a094b3d3cd4c25dca0a1206f0940c1a6933ee6
Guido Martínez <mtzguido@gmail.com>
2024-10-07 18:46:26 -0700

    action warnings

--- scripts/format_for_github_actions.sh
+++ scripts/format_for_github_actions.sh
@@ -6,7 +6,6 @@ while read msg; do
   if ! echo "$msg" | grep -q '^{'; then
     continue
   fi
-  echo "chec"
   if [ x$(echo "$msg" | jq .level) == 'x"Error"' ]; then
     file=$(echo "$msg" | jq .range.def.file_name)
     line=$(echo "$msg" | jq .range.def.start_pos.line)
