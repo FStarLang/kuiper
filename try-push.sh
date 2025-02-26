6c7972f17f36ed0307a051897f71ff7e105b2b27
Guido Martínez <mtzguido@gmail.com>
2025-02-26 15:06:39 -0800

    advance yml

--- try-push.sh
+++ try-push.sh
@@ -2,16 +2,6 @@
 
 for p in FStar karamel pulse; do
 	pushd $p
-	if ! git remote | grep -q upstream; then
-		echo "Adding upstream remote"
-		git remote add upstream https://github.com/FStarLang/$p
-	fi
-
-	# if [ $p == pulse ]; then
-	#         br=main
-	# else
-	#         br=master
-	# fi
 
 	echo "$ git push -f origin HEAD:gpu"
 	git push -f origin HEAD:gpu
