960307447edd7874236c09a066daa17a9a769da7
Guido Martínez <mtzguido@gmail.com>
2025-03-15 23:50:54 -0700

    more test coverage

---
+++ test/Test_Kuiper_MatMul_Tiled__U32.cu
@@ -0,0 +1,9 @@
+#include "Kuiper_MatMul_Tiled.h"
+
+#define stem         Kuiper_MatMul_Tiled_matmul_
+#define et           uint32_t
+#define et_lbl       u32
+#define PRIet        PRIu32
+#define EXACT        1
+
+#include "tiled_matmul_driver.c.inc"
