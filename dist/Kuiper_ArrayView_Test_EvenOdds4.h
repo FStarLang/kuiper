279d575c37d9b082568b3a8e549aec0a9aec2680
Guido Martínez <mtzguido@gmail.com>
2025-08-12 15:51:37 -0700

    dist

---
+++ dist/Kuiper_ArrayView_Test_EvenOdds4.h
@@ -0,0 +1,22 @@
+
+
+#ifndef Kuiper_ArrayView_Test_EvenOdds4_H
+#define Kuiper_ArrayView_Test_EvenOdds4_H
+
+#include <kuiper.h>
+
+extern uint32_t Kuiper_ArrayView_Test_EvenOdds4_x;
+
+uint32_t Kuiper_ArrayView_Test_EvenOdds4_foo_even(uint32_t *a);
+
+uint32_t Kuiper_ArrayView_Test_EvenOdds4_foo_odd(uint32_t *a);
+
+uint32_t Kuiper_ArrayView_Test_EvenOdds4_foo_even_over_raw(uint32_t *a);
+
+uint32_t Kuiper_ArrayView_Test_EvenOdds4_foo_odd_over_raw(uint32_t *a);
+
+uint32_t Kuiper_ArrayView_Test_EvenOdds4_test_over_raw(uint32_t *a);
+
+
+#define Kuiper_ArrayView_Test_EvenOdds4_H_DEFINED
+#endif /* Kuiper_ArrayView_Test_EvenOdds4_H */
