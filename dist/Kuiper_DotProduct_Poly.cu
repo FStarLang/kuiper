119ab5f9d5e34a884b99b9e630eda97a074769e6
Guido Martínez <mtzguido@gmail.com>
2025-08-15 17:07:13 -0700

    update dist

--- dist/Kuiper_DotProduct_Poly.cu
+++ dist/Kuiper_DotProduct_Poly.cu
@@ -6,8 +6,3 @@ size_t Kuiper_DotProduct_Poly_m_size = (size_t)1024U;
 
 size_t Kuiper_DotProduct_Poly_size = (size_t)1024U;
 
-size_t Kuiper_DotProduct_Poly_uint32_to_sizet(uint32_t x)
-{
-  return (size_t)x;
-}
-
--- dist/Kuiper_DotProduct_Poly.h
+++ dist/Kuiper_DotProduct_Poly.h
@@ -9,8 +9,6 @@ extern size_t Kuiper_DotProduct_Poly_m_size;
 
 extern size_t Kuiper_DotProduct_Poly_size;
 
-size_t Kuiper_DotProduct_Poly_uint32_to_sizet(uint32_t x);
-
 
 #define Kuiper_DotProduct_Poly_H_DEFINED
 #endif /* Kuiper_DotProduct_Poly_H */
