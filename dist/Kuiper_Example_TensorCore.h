87470121e02cd97145827055418a796b83514d1d
Guido Martínez <mtzguido@gmail.com>
2025-08-19 15:56:20 -0700

    nit in fixup.sed, try to unfold kpr_offset

--- dist/Kuiper_Example_TensorCore.h
+++ dist/Kuiper_Example_TensorCore.h
@@ -10,6 +10,11 @@ inline
 __device__
 void Kuiper_Example_TensorCore_test(half_t *m1, half_t *m2, half_t *m3);
 
+inline
+
+__device__
+void Kuiper_Example_TensorCore_test2(half_t *m1, half_t *m2, half_t *m3);
+
 
 #define Kuiper_Example_TensorCore_H_DEFINED
 #endif /* Kuiper_Example_TensorCore_H */
