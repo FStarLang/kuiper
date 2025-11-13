6733bd814caca03234af9833be8a4f239cb36c79
Guido Martínez <mtzguido@gmail.com>
2025-11-13 14:38:45 -0800

    handwritten bench support

---
+++ handwritten-bench/BlockTiling2D.h
@@ -0,0 +1,11 @@
+void
+blocktiling2d_host(
+    float_t alpha,
+    float_t beta,
+    uint32_t rows,
+    uint32_t shared,
+    uint32_t cols,
+    float_t *gA,
+    float_t *gB,
+    float_t *gC
+);
