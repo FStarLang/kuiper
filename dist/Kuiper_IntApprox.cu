57b4f1ebaa14ebe23f5484fae27148dad2df372b
Guido Martínez <mtzguido@gmail.com>
2025-11-16 12:48:58 -0800

    update dist

--- dist/Kuiper_IntApprox.cu
+++ dist/Kuiper_IntApprox.cu
@@ -1,4 +1,4 @@
 
 #include "Kuiper_IntApprox.h"
 
-static void *real_like_u32 = (void *)0U;
+uint32_t Kuiper_IntApprox_x = 1U;
--- dist/Kuiper_IntApprox.h
+++ dist/Kuiper_IntApprox.h
@@ -4,18 +4,7 @@
 
 #include <kuiper.h>
 
-typedef struct Kuiper_Sized_sized__uint32_t_s {
-    uint32_t size;
-    uint32_t default;
-} Kuiper_Sized_sized__uint32_t;
-
-typedef struct Kuiper_Scalars_scalar__uint32_t_s {
-    Kuiper_Sized_sized__uint32_t is_sized;
-     uint32_t(*add) (uint32_t x0, uint32_t x1);
-     uint32_t(*mul) (uint32_t x0, uint32_t x1);
-    uint32_t zero;
-    uint32_t one;
-} Kuiper_Scalars_scalar__uint32_t;
+extern uint32_t Kuiper_IntApprox_x;
 
 #define Kuiper_IntApprox_H_DEFINED
 #endif                          /* Kuiper_IntApprox_H */
