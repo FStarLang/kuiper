ad273b1f7545971d985b633f211f054b73513f9f
Guido Martínez <mtzguido@gmail.com>
2025-11-25 11:07:55 -0800

    update dist

--- dist/TestFor.h
+++ dist/TestFor.h
@@ -8,5 +8,11 @@ void TestFor_g(uint32_t x);
 
 void TestFor_test(void);
 
+void TestFor_test_nested(void);
+
+void TestFor_test_nested_lit(void);
+
+void TestFor_test_nested_lit_shadowed(void);
+
 #define TestFor_H_DEFINED
 #endif                          /* TestFor_H */
