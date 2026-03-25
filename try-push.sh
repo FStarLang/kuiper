65e1d3e7cb636ed3e8efab46c95688fc0176bb31
Guido Martínez <mtzguido@gmail.com>
2026-03-25 08:09:28 -0700

    Push to _kuiper branches, avoid CI

--- try-push.sh
+++ try-push.sh
@@ -5,8 +5,8 @@ set -eux
 for p in FStar karamel; do
 	pushd $p
 
-	echo "$ git push -f origin HEAD:refs/heads/kuiper"
-	git push -f origin HEAD:refs/heads/kuiper
+	echo "$ git push -f origin HEAD:refs/heads/_kuiper"
+	git push -f origin HEAD:refs/heads/_kuiper
 
 	popd
 done
