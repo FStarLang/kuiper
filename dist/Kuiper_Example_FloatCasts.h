4a2bee37f25c511bef5e1882ee6e7401bef9655a
Guido Martínez <mtzguido@gmail.com>
2026-06-22 08:51:15 -0700

    dist

--- dist/Kuiper_Example_FloatCasts.h
+++ dist/Kuiper_Example_FloatCasts.h
@@ -22,5 +22,19 @@ float Kuiper_Example_FloatCasts_test_cast_f64_f32(double x);
 
 double Kuiper_Example_FloatCasts_test_cast_f64_f64(double x);
 
+__nv_bfloat16 Kuiper_Example_FloatCasts_test_cast_bf16_bf16(__nv_bfloat16 x);
+
+float Kuiper_Example_FloatCasts_test_cast_bf16_f32(__nv_bfloat16 x);
+
+__nv_bfloat16 Kuiper_Example_FloatCasts_test_cast_f32_bf16(float x);
+
+__nv_bfloat16 Kuiper_Example_FloatCasts_test_cast_f16_bf16(half x);
+
+half Kuiper_Example_FloatCasts_test_cast_bf16_f16(__nv_bfloat16 x);
+
+double Kuiper_Example_FloatCasts_test_cast_bf16_f64(__nv_bfloat16 x);
+
+__nv_bfloat16 Kuiper_Example_FloatCasts_test_cast_f64_bf16(double x);
+
 #define Kuiper_Example_FloatCasts_H_DEFINED
 #endif                          /* Kuiper_Example_FloatCasts_H */
